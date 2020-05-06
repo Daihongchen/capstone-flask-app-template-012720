290ba6dbc1c113c11c0a4abf81b1ccdb142be6b3
DaihongChen <daihongchen@icloud.com>
2020-05-06 14:24:09 -0700

    add more

---
+++ src/models/movie_recommend.py
@@ -0,0 +1,49 @@
+import tensorflow as tf
+from tensorflow.keras.models import load_model
+from tensorflow.keras.models import model_from_json
+from tensorflow import keras
+import pandas as pd 
+import numpy as np
+import json
+import json_config
+pd.set_option('display.float_format', '{:.1f}'.format)
+
+def recommender(test_value):
+
+    # Reload the model from the 2 files we saved
+    # with open('src/models/model_config.json') as json_file:
+    #     json_config = json_file.read()
+    # model_mr = tf.keras.models.model_from_json(json_config) 
+    # # # Load weights
+    # model_mr.load_weights('src/models/weights_only.h5') 
+    # load the model
+    model_mr = tf.keras.models.load_model('src/models/mr_model.h5')
+    
+    ## pull the data for deploy
+    data_mr = pd.read_csv('src/models/data_2018_mr.csv')
+
+    movie_data = np.array(list(set(data_mr.movie)))
+    user = np.array([test_value for i in range(len(movie_data))])
+    
+    predictions = model_mr.predict([user, movie_data])
+    predictions = np.array([a[0] for a in predictions])
+    recommended_movie_ids = (-predictions).argsort()[:5]
+
+    recommend = data_mr[data_mr['movie'].isin(recommended_movie_ids)]
+    recommend = recommend[[
+                            'title', 
+                            'average_rating',
+                            'category', 
+                            'description', 
+                            'price', 
+                            'links']].drop_duplicates()
+    recommend = recommend.rename(columns={'title':'Title',
+                                          'average_rating':'Overall_Rating',
+                                          'category': 'Category',
+                                          'description':'Description',
+                                          'price': 'Price',
+                                          'links': 'Links'})
+    
+    return recommend 
+
+
---
+++ src/utils_mr.py
@@ -0,0 +1,19 @@
+def validate_input(data):
+    test_value = []
+    errors = []
+    
+    EXPECTED_FEATURES = ("UserID",)
+    
+    if not data:
+        errors.append("Form data must not be empty")
+    else:
+        for feature in EXPECTED_FEATURES:
+            if feature not in data:
+                errors.append(f"'{feature}' is a required field")
+            else:
+                try:
+                    test_value.append(float(data[feature]))
+                except ValueError:
+                    errors.append(f"Invalid value for field {feature}: '{data[feature]}'")
+
+    return test_value, errors
